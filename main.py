782fbbfccc264c3dfb35113c66826900fab1ae70
Josh-Green <joshgreen95@protonmail.com>
2024-05-30 00:30:19 +0100

    Subs Updated
    generate subtitles now correctly generates subtitles using timings gathered from openai's whisper.

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
 import os
 
-import modules.reddit_scraper.redditscraper as redditscraper
-import modules.voice_generation.generatevoice as generatevoice
+# import modules.reddit_scraper.redditscraper as redditscraper
+# import modules.voice_generation.generatevoice as generatevoice
 import modules.video_editor.videoeditor as videoeditor
 
 scriptDir = os.path.dirname(os.path.abspath(__file__))
 videoFilePath = os.path.join(scriptDir, 'videos')
 
 #Scrape Reddit and Return folder of stored stories
-detailsArray = redditscraper.Scrape('SHOWERTHOUGHTS', 1, scriptDir)
+#detailsArray = redditscraper.Scrape('SHOWERTHOUGHTS', 1, scriptDir)
 
-detailsArray = generatevoice.GenerateVoice(detailsArray)
+#detailsArray = generatevoice.GenerateVoice(detailsArray)
 
 # #Test Array - No need to gen a voice everytime
-# detailsArray = [{'folderLocation': 'f:\\Projects\\reddit-scraper\\reddit-scraper\\posts\\8d33f140-1b8d-11ef-bd0b-00933795e326', 
-#                 'textFilePath': 'f:\\Projects\\reddit-scraper\\reddit-scraper\\posts\\8d33f140-1b8d-11ef-bd0b-00933795e326\\Whoever_created_the_.txt',
-#                 'audioPath': 'f:\\Projects\\reddit-scraper\\reddit-scraper\\posts\\8d33f140-1b8d-11ef-bd0b-00933795e326\\Whoever_created_the_.txt.mp3'}]
+detailsArray = [{'folderLocation': 'f:\\Projects\\reddit-scraper\\reddit-scraper\\posts\\8d33f140-1b8d-11ef-bd0b-00933795e326', 
+                 'textFilePath': 'f:\\Projects\\reddit-scraper\\reddit-scraper\\posts\\8d33f140-1b8d-11ef-bd0b-00933795e326\\Whoever_created_the_.txt',
+                 'audioPath': 'f:\\Projects\\reddit-scraper\\reddit-scraper\\posts\\8d33f140-1b8d-11ef-bd0b-00933795e326\\Whoever_created_the_.txt.mp3'}]
 
 for details in detailsArray:
     videoeditor.Edit(details, videoFilePath, '.mp4')
--- modules/video_editor/generatesubtitles.py
+++ modules/video_editor/generatesubtitles.py
@@ -1,73 +1,60 @@
 import os
+
+from pydub import AudioSegment
 from moviepy.editor import VideoFileClip, TextClip, CompositeVideoClip
 
-
-import pickle
+import whisper
 
 def GenerateSubtitles(details, videoClip):
-    def ReadTextFile(details):
-        with open(details['textFilePath'], 'r') as file:
-            lines = file.readlines()
+    font = 'arial'
+    fontSize = 42
+    color = 'white'
+    backgroundColor = None
+    subtitlePosition = ('center')
 
-        transcript = ' '.join([line.strip() for line in lines if line.strip()])
-        transcript = transcript.split()
-        
-        return transcript
-    
-    def CalculateDurationPerWord(words, videoClip):
-        word_factor = 0.2
-        video_factor = 0.8
-        videoDuration = videoClip.duration
+    def Transcribe(path):
+        model = whisper.load_model("base")
+        results = model.transcribe(path, word_timestamps=True)
+        return results
 
-        totalWords = len(words)
-        wordDurations = []
+    def ExtractTimings(results):
+        wordTimings = []
 
-        for word in words:
-            duration = (len(word) * word_factor + videoDuration * video_factor) / totalWords
-            wordDurations.append(duration)
+        for segment in results['segments']:
+            words = segment.get('words', None)
+            if words:
+                for wordInfo in words:
+                    startTime = wordInfo['start']
+                    endTime = wordInfo['end']
+                    word = wordInfo['word']
 
-        return wordDurations
-    
-    def CreateWordClips(words, wordDurations, videoClip):
-        wordClips = []
-        currentTime = 0
+                    wordTimings.append((startTime, endTime, word))
+        return wordTimings
 
-        fontSize = 64
-        color = 'white'
-        font = 'Arial'
+    def GenerateTextClips(wordTImings):
+        textClips = []
 
-        for word, duration in zip(words, wordDurations):
-            textClip = TextClip(word, fontsize=fontSize, color=color, font=font)
-            textClip = textClip.set_position(('center'))
+        for startTime, endTime, word in wordTImings:
+            duration = endTime - startTime
+            textClip = TextClip(word, 
+                                font=font,
+                                fontsize=fontSize,
+                                color=color,
+                                )
+            textClip = textClip.set_position(subtitlePosition)
+            textClip = textClip.set_start(startTime)
             textClip = textClip.set_duration(duration)
-            textClip = textClip.set_start(currentTime)
 
-            wordClips.append(textClip)
-            currentTime += duration
-        
-        return wordClips
-    
-    def TranscribeAudio(audioFile):
-        recognizer = sr.Recognizer()
-        audio = AudioSegment.from_file(audioFile)
+            textClips.append(textClip)
+        return textClips 
 
-        segments = split_on_silence(audio, min_silence_len=500, silence_thresh=-40)
-        transcript = []
+    audio = AudioSegment.from_mp3(details['audioPath'])
+    audio.export('temp.wav', format='wav')
+    transcription = Transcribe('temp.wav')
+    wordTimings = ExtractTimings(transcription)
+    textClips = GenerateTextClips(wordTimings)
 
-        for segment in segments:
-            with sr.AudioFile(segment) as source:
-                audioData = recognizer.record(source)
-                text = recognizer.recognize_google(audioData)
-                transcript.append(text)
+    finalVideo = CompositeVideoClip([videoClip] + textClips)
+    return finalVideo
 
-        return ' '.join(transcript).split()
 
-    audioTranscript = TranscribeAudio(details['audioPath'])
-    
-    transcript = ReadTextFile(details)
-    durations = CalculateDurationPerWord(audioTranscript, videoClip)
-    wordClips = CreateWordClips(audioTranscript, durations, videoClip)
-    
-    videoWithSubtitles = CompositeVideoClip([videoClip] + wordClips)
-
-    return videoWithSubtitles;
